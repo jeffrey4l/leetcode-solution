7b597e846090e28a7edf3bafab1bef7ef2b3d9f4
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-12-24 14:14:06 +0800

    add two sum solution

---
+++ solution/two_sum.go
@@ -0,0 +1,45 @@
+// Package twosum problem: https://leetcode.com/problems/two-sum/solution/
+package twosum
+
+func twoSum1(nums []int, target int) []int {
+
+	for index, v := range nums {
+		need := target - v
+		for startIndex := index + 1; startIndex < len(nums); startIndex++ {
+			if need == nums[startIndex] {
+				return []int{index, startIndex}
+			}
+		}
+
+	}
+	return []int{}
+}
+func twoSum2(nums []int, target int) []int {
+
+	indexes := make(map[int]int, len(nums))
+	for index, v := range nums {
+		indexes[v] = index
+	}
+
+	for index, v := range nums {
+		needed := target - v
+		if foundedIndex, ok := indexes[needed]; ok && foundedIndex != index {
+			return []int{index, foundedIndex}
+		}
+
+	}
+	return []int{}
+}
+func twoSum3(nums []int, target int) []int {
+
+	indexes := make(map[int]int, len(nums))
+
+	for index, v := range nums {
+		if foundedIndex, ok := indexes[target-v]; ok {
+			return []int{index, foundedIndex}
+		}
+		indexes[v] = index
+
+	}
+	return []int{}
+}
